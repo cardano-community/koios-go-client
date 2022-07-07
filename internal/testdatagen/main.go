c109f379a088e26d7d9b0a22e29cad6c8ba65f73
Marko Kungla <marko.kungla@gmail.com>
2022-07-08 02:09:59 +0300

    test: update block testdata
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- internal/testdatagen/main.go
+++ internal/testdatagen/main.go
@@ -290,7 +290,7 @@ func specs() []internal.APITestSpec {
 			Endpoint: "/block_info",
 			Request: internal.APITestRequestSpec{
 				Method: "POST",
-				Body:   []byte("{\"_block_hashes\": [\"fb9087c9f1408a7bbd7b022fd294ab565fec8dd3a8ef091567482722a1fa4e30\"]}"),
+				Body:   []byte("{\"_block_hashes\": [\"fb9087c9f1408a7bbd7b022fd294ab565fec8dd3a8ef091567482722a1fa4e30\",\"60188a8dcb6db0d80628815be2cf626c4d17cb3e826cebfca84adaff93ad492a\",\"c6646214a1f377aa461a0163c213fc6b86a559a2d6ebd647d54c4eb00aaab015\"]}"),
 			},
 		},
 		{
