558fb6077ac683f53f4e8f5369b3b06cf5bee532
Marko Kungla <marko.kungla@gmail.com>
2022-10-01 16:59:48 +0300

    test: make endpoint tests reusable
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- internal/testsuite/network_test.go
+++ internal/testsuite/network_test.go
@@ -17,11 +17,8 @@
 package testsuite
 
 import (
-	"context"
-	"os"
 	"testing"
 
-	"github.com/cardano-community/koios-go-client/v2"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,17 +27,7 @@ func TestNetworkTip(t *testing.T) {
 	if !assert.NoError(t, err) {
 		return
 	}
-
-	tip, err := client.GetTip(context.Background(), nil)
-	assert.NoError(t, err)
-
-	assert.Greater(t, tip.Data.AbsSlot, int(100000))
-	assert.Greater(t, tip.Data.BlockNo, int(100000))
-	assert.Greater(t, tip.Data.EpochNo, int(230))
-	assert.Greater(t, tip.Data.EpochSlot, int(1))
-	assert.NotEmpty(t, tip.Data.BlockHash)
-	assert.NotZero(t, tip.Data.BlockTime)
-	assert.False(t, tip.Data.BlockTime.IsZero())
+	NetworkTipTest(t, client)
 }
 
 func TestNetworkGenesis(t *testing.T) {
@@ -48,23 +35,7 @@ func TestNetworkGenesis(t *testing.T) {
 	if !assert.NoError(t, err) {
 		return
 	}
-
-	res, err := client.GetGenesis(context.Background(), nil)
-	if !assert.NoError(t, err) {
-		return
-	}
-	assert.True(t, res.Data.NetworkMagic.IsPositive())
-	assert.NotEmpty(t, res.Data.NetworkID)
-	assert.True(t, res.Data.EpochLength.IsPositive())
-	assert.True(t, res.Data.SlotLength.IsPositive())
-	assert.True(t, res.Data.MaxLovelaceSupply.IsPositive())
-	assert.False(t, res.Data.SystemStart.IsZero())
-	assert.True(t, res.Data.ActiveSlotCoeff.IsPositive())
-	assert.True(t, res.Data.SlotsPerKesPeriod.IsPositive())
-	assert.True(t, res.Data.MaxKesRevolutions.IsPositive())
-	assert.True(t, res.Data.SecurityParam.IsPositive())
-	assert.True(t, res.Data.UpdateQuorum.IsPositive())
-	assert.NotEmpty(t, res.Data.AlonzoGenesis)
+	NetworkGenesisTest(t, client)
 }
 
 func TestNetworkTotals(t *testing.T) {
@@ -72,26 +43,5 @@ func TestNetworkTotals(t *testing.T) {
 	if !assert.NoError(t, err) {
 		return
 	}
-
-	var epoch koios.EpochNo
-	switch os.Getenv("KOIOS_NETWORK") {
-	case "mainnet":
-		epoch = koios.EpochNo(320)
-	case "guild":
-		epoch = koios.EpochNo(1950)
-	case "testnet":
-		epoch = koios.EpochNo(185)
-	}
-
-	res, err := client.GetTotals(context.Background(), &epoch, nil)
-	if !assert.NoError(t, err) {
-		return
-	}
-	zero := int64(0)
-	assert.Equal(t, epoch, res.Data[0].EpochNo)
-	assert.Greater(t, res.Data[0].Circulation.IntPart(), zero)
-	assert.Greater(t, res.Data[0].Reserves.IntPart(), zero)
-	assert.Greater(t, res.Data[0].Reward.IntPart(), zero)
-	assert.Greater(t, res.Data[0].Supply.IntPart(), zero)
-	assert.Greater(t, res.Data[0].Treasury.IntPart(), zero)
+	NetworkTotalsTest(t, client)
 }
