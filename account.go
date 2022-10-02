0d825c143aa1388d4706ae6800e8d8099b11ddc0
Marko Kungla <marko.kungla@gmail.com>
2022-10-02 15:39:47 +0300

    feat: enhance accounts api
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- account.go
+++ account.go
@@ -18,6 +18,9 @@ package koios
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 
 	"github.com/shopspring/decimal"
 )
@@ -26,7 +29,8 @@ type (
 	// AccountInfo data returned by `/account_info`.
 	AccountInfo struct {
 		Status           string          `json:"status"`
-		DelegatedPool    PoolID          `json:"delegated_pool"`
+		DelegatedPool    *PoolID         `json:"delegated_pool"`
+		StakeAddress     Address         `json:"stake_address"`
 		TotalBalance     decimal.Decimal `json:"total_balance"`
 		UTxO             decimal.Decimal `json:"utxo"`
 		Rewards          decimal.Decimal `json:"rewards"`
@@ -36,7 +40,12 @@ type (
 		Treasury         decimal.Decimal `json:"treasury"`
 	}
 
-	// AccountRewards data returned by `/account_rewards`.
+	// AccountRewardsInfo data returned by `/account_rewards`.
+	AccountRewardsInfo struct {
+		StakeAddress Address          `json:"stake_address"`
+		Rewards      []AccountRewards `json:"rewards"`
+	}
+
 	AccountRewards struct {
 		PoolID         PoolID          `json:"pool_id"`
 		EarnedEpoch    EpochNo         `json:"earned_epoch"`
@@ -46,29 +55,14 @@ type (
 	}
 
 	// AccountHistoryEntry history entry list item.
+	AccountHistory struct {
+		StakeAddress Address               `json:"stake_address"`
+		History      []AccountHistoryEntry `json:"history"`
+	}
 	AccountHistoryEntry struct {
-		StakeAddress Address         `json:"stake_address"`
-		PoolID       PoolID          `json:"pool_id"`
-		Epoch        EpochNo         `json:"epoch_no"`
-		ActiveStake  decimal.Decimal `json:"active_stake"`
-	}
-
-	// AccountAsset asset list item when requesting account info.
-	AccountAsset struct {
-		// Name Asset Name (hex).
-		Name string `json:"asset_name"`
-
-		// PolicyID Asset Policy ID (hex).
-		PolicyID PolicyID `json:"asset_policy"`
-
-		// Quantity of assets
-		Quantity decimal.Decimal `json:"quantity"`
-	}
-
-	// AccountAction data entry for `/account_updates`.
-	AccountAction struct {
-		ActionType string `json:"action_type"`
-		TxHash     TxHash `json:"tx_hash"`
+		PoolID      PoolID          `json:"pool_id"`
+		EpochNo     EpochNo         `json:"epoch_no"`
+		ActiveStake decimal.Decimal `json:"active_stake"`
 	}
 
 	// AccountListResponse represents response from `/account_list` endpoint.
@@ -83,34 +77,85 @@ type (
 		Data *AccountInfo `json:"data"`
 	}
 
+	AccountsInfoResponse struct {
+		Response
+		Data []AccountInfo `json:"data"`
+	}
+
 	// AccountRewardsResponse represents response from `/account_rewards` endpoint.
 	AccountRewardsResponse struct {
 		Response
-		Data []AccountRewards `json:"data"`
+		Data *AccountRewardsInfo `json:"data"`
+	}
+	AccountsRewardsResponse struct {
+		Response
+		Data []AccountRewardsInfo `json:"data"`
+	}
+
+	// AccountAction data entry for `/account_updates`.
+	AccountUpdate struct {
+		ActionType   string    `json:"action_type"`
+		TxHash       TxHash    `json:"tx_hash"`
+		EpochNo      EpochNo   `json:"epoch_no"`
+		EpochSlot    Slot      `json:"epoch_slot"`
+		AbsoluteSlot Slot      `json:"absolute_slot"`
+		BlockTime    Timestamp `json:"block_time"`
+	}
+
+	AccountUpdates struct {
+		StakeAddress Address         `json:"stake_address"`
+		Updates      []AccountUpdate `json:"updates"`
 	}
 
 	// AccountUpdatesResponse represents response from `/account_rewards` endpoint.
 	AccountUpdatesResponse struct {
 		Response
-		Data []AccountAction `json:"data"`
+		Data *AccountUpdates `json:"data"`
 	}
 
+	AccountsUpdatesResponse struct {
+		Response
+		Data []AccountUpdates `json:"data"`
+	}
+
+	AccountAddresses struct {
+		StakeAddress Address   `json:"stake_address"`
+		Addresses    []Address `json:"addresses"`
+	}
 	// AccountAddressesResponse represents response from `/account_addresses` endpoint.
 	AccountAddressesResponse struct {
 		Response
-		Data []Address `json:"data"`
+		Data *AccountAddresses `json:"data"`
+	}
+	AccountsAddressesResponse struct {
+		Response
+		Data []AccountAddresses `json:"data"`
 	}
 
 	// AccountAssetsResponse represents response from `/account_assets` endpoint.
 	AccountAssetsResponse struct {
 		Response
-		Data []AccountAsset `json:"data"`
+		Data *AccountAssets `json:"data"`
+	}
+
+	AccountsAssetsResponse struct {
+		Response
+		Data []AccountAssets `json:"data"`
+	}
+
+	AccountAssets struct {
+		StakeAddress Address             `json:"stake_address"`
+		Assets       []AddressCollection `json:"assets"`
 	}
 
 	// AccountHistoryResponse represents response from `/account_history` endpoint.
 	AccountHistoryResponse struct {
 		Response
-		Data []AccountHistoryEntry `json:"data"`
+		Data *AccountHistory `json:"data"`
+	}
+	AccountsHistoryResponse struct {
+		Response
+		Data []AccountHistory `json:"data"`
 	}
 )
 
@@ -140,35 +185,39 @@ func (c *Client) GetAccountList(
 }
 
 // GetAccountInfo returns the account info of any (payment or staking) address.
-
 func (c *Client) GetAccountInfo(
 	ctx context.Context,
-	addr Address,
+	acc Address,
 	opts *RequestOptions,
 ) (res *AccountInfoResponse, err error) {
-	res = &AccountInfoResponse{}
-
-	if _, err = addr.Valid(); err != nil {
-		res.applyError(nil, err)
-		return
-	}
-
-	if opts == nil {
-		opts = c.NewRequestOptions()
-	}
-	opts.QuerySet("_address", addr.String())
-
-	rsp, err := c.request(ctx, &res.Response, "GET", "/account_info", nil, opts)
+	res2, err := c.GetAccountsInfo(ctx, []Address{acc}, opts)
 	if err != nil {
 		return
 	}
-
-	addrs := []AccountInfo{}
-	err = ReadAndUnmarshalResponse(rsp, &res.Response, &addrs)
-
-	if len(addrs) == 1 {
-		res.Data = &addrs[0]
+	if len(res2.Data) == 1 {
+		res.Data = &res2.Data[0]
+	} else {
+		return nil, fmt.Errorf("%w: no account info found for address %s", ErrNoData, acc)
 	}
+	return
+}
+
+func (c *Client) GetAccountsInfo(
+	ctx context.Context,
+	accs []Address,
+	opts *RequestOptions,
+) (res *AccountsInfoResponse, err error) {
+	res = &AccountsInfoResponse{}
+	if len(accs) == 0 {
+		err = ErrNoAddress
+		res.applyError(nil, err)
+		return
+	}
+	rsp, err := c.request(ctx, &res.Response, "POST", "/account_info", stakeAddressesPL(accs, nil), opts)
+	if err != nil {
+		return
+	}
+	err = ReadAndUnmarshalResponse(rsp, &res.Response, &res.Data)
 	return
 }
 
@@ -176,27 +225,37 @@ func (c *Client) GetAccountInfo(
 // for a stake address, or certain epoch if specified.
 func (c *Client) GetAccountRewards(
 	ctx context.Context,
-	addr Address,
+	acc Address,
 	epoch *EpochNo,
 	opts *RequestOptions,
 ) (res *AccountRewardsResponse, err error) {
 	res = &AccountRewardsResponse{}
 
-	if _, err = addr.Valid(); err != nil {
+	res2, err := c.GetAccountsRewards(ctx, []Address{acc}, epoch, opts)
+	if err != nil {
+		return
+	}
+	if len(res2.Data) == 1 {
+		res.Data = &res2.Data[0]
+	} else {
+		return nil, fmt.Errorf("%w: no rewards found for account %s", ErrNoData, acc)
+	}
+	return
+}
+
+func (c *Client) GetAccountsRewards(
+	ctx context.Context,
+	accs []Address,
+	epoch *EpochNo,
+	opts *RequestOptions,
+) (res *AccountsRewardsResponse, err error) {
+	res = &AccountsRewardsResponse{}
+	if len(accs) == 0 {
+		err = ErrNoAddress
 		res.applyError(nil, err)
 		return
 	}
-
-	if opts == nil {
-		opts = c.NewRequestOptions()
-	}
-	opts.QuerySet("_stake_address", addr.String())
-
-	if epoch != nil {
-		opts.QuerySet("_epoch_no", epoch.String())
-	}
-
-	rsp, err := c.request(ctx, &res.Response, "GET", "/account_rewards", nil, opts)
+	rsp, err := c.request(ctx, &res.Response, "POST", "/account_rewards", stakeAddressesPL(accs, epoch), opts)
 	if err != nil {
 		return
 	}
@@ -206,24 +265,39 @@ func (c *Client) GetAccountRewards(
 
 // GetAccountUpdates (History) retruns the account updates
 // (registration, deregistration, delegation and withdrawals).
+
 func (c *Client) GetAccountUpdates(
 	ctx context.Context,
-	addr Address,
+	acc Address,
+	epoch *EpochNo,
 	opts *RequestOptions,
 ) (res *AccountUpdatesResponse, err error) {
 	res = &AccountUpdatesResponse{}
 
-	if _, err = addr.Valid(); err != nil {
+	res2, err := c.GetAccountsUpdates(ctx, []Address{acc}, opts)
+	if err != nil {
+		return
+	}
+	if len(res2.Data) == 1 {
+		res.Data = &res2.Data[0]
+	} else {
+		return nil, fmt.Errorf("%w: no updates found for account %s", ErrNoData, acc)
+	}
+	return
+}
+
+func (c *Client) GetAccountsUpdates(
+	ctx context.Context,
+	accs []Address,
+	opts *RequestOptions,
+) (res *AccountsUpdatesResponse, err error) {
+	res = &AccountsUpdatesResponse{}
+	if len(accs) == 0 {
+		err = ErrNoAddress
 		res.applyError(nil, err)
 		return
 	}
-
-	if opts == nil {
-		opts = c.NewRequestOptions()
-	}
-	opts.QuerySet("_stake_address", addr.String())
-
-	rsp, err := c.request(ctx, &res.Response, "GET", "/account_updates", nil, opts)
+	rsp, err := c.request(ctx, &res.Response, "POST", "/account_updates", stakeAddressesPL(accs, nil), opts)
 	if err != nil {
 		return
 	}
@@ -234,58 +308,76 @@ func (c *Client) GetAccountUpdates(
 // GetAccountAddresses retruns all addresses associated with an account.
 func (c *Client) GetAccountAddresses(
 	ctx context.Context,
-	addr Address,
+	acc Address,
+	epoch *EpochNo,
 	opts *RequestOptions,
 ) (res *AccountAddressesResponse, err error) {
 	res = &AccountAddressesResponse{}
 
-	if _, err = addr.Valid(); err != nil {
-		res.applyError(nil, err)
-		return res, err
-	}
-
-	if opts == nil {
-		opts = c.NewRequestOptions()
-	}
-	opts.QuerySet("_address", addr.String())
-
-	rsp, err := c.request(ctx, &res.Response, "GET", "/account_addresses", nil, opts)
+	res2, err := c.GetAccountsAddresses(ctx, []Address{acc}, opts)
 	if err != nil {
-		return res, err
+		return
 	}
-	addrs := []struct {
-		Addr Address `json:"address"`
-	}{}
-
-	err = ReadAndUnmarshalResponse(rsp, &res.Response, &addrs)
-
-	if len(addrs) > 0 {
-		for _, a := range addrs {
-			res.Data = append(res.Data, a.Addr)
-		}
+	if len(res2.Data) == 1 {
+		res.Data = &res2.Data[0]
+	} else {
+		return nil, fmt.Errorf("%w: no updates found for account %s", ErrNoData, acc)
 	}
-	return res, err
+	return
+}
+
+func (c *Client) GetAccountsAddresses(
+	ctx context.Context,
+	accs []Address,
+	opts *RequestOptions,
+) (res *AccountsAddressesResponse, err error) {
+	res = &AccountsAddressesResponse{}
+	if len(accs) == 0 {
+		err = ErrNoAddress
+		res.applyError(nil, err)
+		return
+	}
+	rsp, err := c.request(ctx, &res.Response, "POST", "/account_addresses", stakeAddressesPL(accs, nil), opts)
+	if err != nil {
+		return
+	}
+	err = ReadAndUnmarshalResponse(rsp, &res.Response, &res.Data)
+	return
 }
 
 // GetAccountAssets retruns all the native asset balance of an account.
 func (c *Client) GetAccountAssets(
 	ctx context.Context,
-	addr Address,
+	acc Address,
+	epoch *EpochNo,
 	opts *RequestOptions,
 ) (res *AccountAssetsResponse, err error) {
 	res = &AccountAssetsResponse{}
 
-	if _, err = addr.Valid(); err != nil {
+	res2, err := c.GetAccountsAssets(ctx, []Address{acc}, opts)
+	if err != nil {
+		return
+	}
+	if len(res2.Data) == 1 {
+		res.Data = &res2.Data[0]
+	} else {
+		return nil, fmt.Errorf("%w: no assets found for account %s", ErrNoData, acc)
+	}
+	return
+}
+
+func (c *Client) GetAccountsAssets(
+	ctx context.Context,
+	accs []Address,
+	opts *RequestOptions,
+) (res *AccountsAssetsResponse, err error) {
+	res = &AccountsAssetsResponse{}
+	if len(accs) == 0 {
+		err = ErrNoAddress
 		res.applyError(nil, err)
 		return
 	}
-
-	if opts == nil {
-		opts = c.NewRequestOptions()
-	}
-	opts.QuerySet("_address", addr.String())
-
-	rsp, err := c.request(ctx, &res.Response, "GET", "/account_assets", nil, opts)
+	rsp, err := c.request(ctx, &res.Response, "POST", "/account_assets", stakeAddressesPL(accs, nil), opts)
 	if err != nil {
 		return
 	}
@@ -296,25 +388,52 @@ func (c *Client) GetAccountAssets(
 // GetAccountHistory retruns the staking history of an account.
 func (c *Client) GetAccountHistory(
 	ctx context.Context,
-	addr Address,
+	acc Address,
+	epoch *EpochNo,
 	opts *RequestOptions,
 ) (res *AccountHistoryResponse, err error) {
 	res = &AccountHistoryResponse{}
 
-	if _, err = addr.Valid(); err != nil {
+	res2, err := c.GetAccountsHistory(ctx, []Address{acc}, opts)
+	if err != nil {
+		return
+	}
+	if len(res2.Data) == 1 {
+		res.Data = &res2.Data[0]
+	} else {
+		return nil, fmt.Errorf("%w: no history found for account %s", ErrNoData, acc)
+	}
+	return
+}
+
+func (c *Client) GetAccountsHistory(
+	ctx context.Context,
+	accs []Address,
+	opts *RequestOptions,
+) (res *AccountsHistoryResponse, err error) {
+	res = &AccountsHistoryResponse{}
+	if len(accs) == 0 {
+		err = ErrNoAddress
 		res.applyError(nil, err)
 		return
 	}
-
-	if opts == nil {
-		opts = c.NewRequestOptions()
-	}
-	opts.QuerySet("_address", addr.String())
-
-	rsp, err := c.request(ctx, &res.Response, "GET", "/account_history", nil, opts)
+	rsp, err := c.request(ctx, &res.Response, "POST", "/account_history", stakeAddressesPL(accs, nil), opts)
 	if err != nil {
 		return
 	}
 	err = ReadAndUnmarshalResponse(rsp, &res.Response, &res.Data)
 	return
+}
+
+func stakeAddressesPL(addrs []Address, epoch *EpochNo) io.Reader {
+	var payload = struct {
+		Adresses []Address `json:"_stake_addresses"`
+		Epoch    *EpochNo  `json:"_epoch_no,omitempty"`
+	}{addrs, epoch}
+	rpipe, w := io.Pipe()
+	go func() {
+		_ = json.NewEncoder(w).Encode(payload)
+		defer w.Close()
+	}()
+	return rpipe
 }
